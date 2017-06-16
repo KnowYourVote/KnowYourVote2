d70f8783a0970a05146dd225a075df8af25d2594
lilykrichels <lily.krichels@gmail.com>
2017-06-16 15:42:06 -0400

    Added Content About Government

--- KnowYourVote2/Models/KnowYourVoteDB.Designer.cs
+++ KnowYourVote2/Models/KnowYourVoteDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\wecancodeit\documents\visual studio 2015\Projects\KnowYourVote2\KnowYourVote2\Models\KnowYourVoteDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\WeCanCodeIT\Documents\Visual Studio 2015\Projects\KnowYourVote2\KnowYourVote2\Models\KnowYourVoteDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
