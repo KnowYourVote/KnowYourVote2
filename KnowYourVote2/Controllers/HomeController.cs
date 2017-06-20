2bee269722c07b0ffba7d2ce2ac27b9ed027b07a
myusername <Lesliekornes@gmail.com>
2017-06-20 13:45:58 -0400

    Added Views for NavBar when logged in

--- KnowYourVote2/Controllers/HomeController.cs
+++ KnowYourVote2/Controllers/HomeController.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace KnowYourVote2.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            return IndexLoggedIn();
+        }
+
+        public ActionResult IndexLoggedIn()
         {
             return View();
         }
