bf7d5c314938a1cb32f6c6506e3d30459d43bf20
NoahSekreter <nsekreter@gmail.com>
2017-06-21 14:13:08 -0400

    Added target _blank to github.io pages

--- KnowYourVote2/Scripts/konami.js
+++ KnowYourVote2/Scripts/konami.js
@@ -4,12 +4,9 @@
     window.addEventListener("keydown", function (e) {
         kkeys.push(e.keyCode);
         if (kkeys.toString().indexOf(konami) >= 0) {
-            document.getElementById('noah').innerHTML = '<img src="/Images/bitmoji-noah.png" /><h2>Noah</h2>'
+            document.getElementById('noah').innerHTML = '<img src="/Images/bitmoji-noah.png" target="_blank"/><h2>Noah</h2>'
             kkeys = [];
         }
-        //if (kkeys.toString().indexOf(konami) >= 0)
-        //    document.getElementById('noah').innerHTML = '<img src="~/Images/bitmoji-noah.png" /><h2>Hi</h2>'
-        //kkeys = [];
     }, true);
 }
 });
